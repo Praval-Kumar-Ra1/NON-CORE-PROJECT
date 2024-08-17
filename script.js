45114f9ce553f4d6e4915659d3575c60267e834d
Praval-Kumar-Ra1 <142446889+Praval-Kumar-Ra1@users.noreply.github.com>
2024-08-17 20:19:59 +0530

    BMI CALCULATOR

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-document.querySelector('.cross').style.display = 'none';
-document.querySelector('.hamburger').addEventListener('click', ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo')
-if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-    document.querySelector('.sidebar').style.display = 'inline'
-    document.querySelector('.cross').style.display = 'none'
-}
-else{
-    document.querySelector('.ham').style.display = 'none'
-    setTimeout(() => {
-        document.querySelector('.cross').style.display = 'inline'
-        
-    }, 300);
-    
+function calculateBMI() {
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
 
+    if (weight && height) {
+        const heightInMeters = height / 100;
+        const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+
+        let resultMessage = `Your BMI is ${bmi}. `;
+
+        if (bmi < 18.5) {
+            resultMessage += "You are underweight.";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            resultMessage += "You have a normal weight.";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            resultMessage += "You are overweight.";
+        } else {
+            resultMessage += "You are obese.";
+        }
+
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerText = resultMessage;
+        resultDiv.classList.add('show');
+    } else {
+        document.getElementById("result").innerText = "Please enter valid values for weight and height.";
+    }
 }
-} )
